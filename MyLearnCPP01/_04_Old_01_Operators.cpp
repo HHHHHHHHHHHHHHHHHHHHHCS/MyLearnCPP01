ae6055eab7540d266d050bceb7102b71ccee274a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-06-13 19:30:09 +0800

    BitwiseOperators

--- MyLearnCPP01/_04_Old_01_Operators.cpp
+++ MyLearnCPP01/_04_Old_01_Operators.cpp
@@ -4,7 +4,10 @@
 void _04_Old_01_Operators::Test()
 {
 	//IncrementDecrement();
-	SizeofCommaConditional();
+	//SizeofCommaConditional();
+	//RelationalOperators();
+	//LogicalOperators();
+	BitwiseOperators();
 }
 
 
@@ -53,18 +56,111 @@ void _04_Old_01_Operators::SizeofCommaConditional()
 	int z = (++x, ++y); // increment x and y
 	std::cout << x << ' ' << y << ' ' << z << std::endl;
 
+	x = 0, y = 2;
+
 	z = (x, y);
 	std::cout << x << ' ' << y << ' ' << z << std::endl;
 
+	x = 0, y = 2;
 	z = x, y;
 	std::cout << x << ' ' << y << ' ' << z << std::endl;
 
+	x = 0, y = 2;
 	z = x > y ? x : y;
 	std::cout << x << ' ' << y << ' ' << z << std::endl;
+
+	x = 0, y = 2;
+	std::cout << ((x > y) ? x : y) << std::endl;
+	//因为 << 运算符的优先级高于？：运算符
+	std::cout << (x > y) ? x : y;
+	//就会变成这样
+	(std::cout << (x > y)) ? x : y;
 }
 
+void _04_Old_01_Operators::RelationalOperators()
+{
+	double d1(100 - 99.99); // should equal 0.01
+	double d2(10 - 9.99); // should equal 0.01
+
+	if (d1 == d2)
+		std::cout << "d1 == d2" << "\n";
+	else if (d1 > d2)
+		std::cout << "d1 > d2" << "\n";
+	else if (d1 < d2)
+		std::cout << "d1 < d2" << "\n";
+
+	std::cout << ApproximatelyEqualAbsRel(d1, d2, 1e-8, 1e-4) << '\n';
+
+	double a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+
+	std::cout << ApproximatelyEqualAbsRel(d1, d2) << "\n";
+
+
+	std::cout << ApproximatelyEqualAbsRel(a - 1.0, 0.0, 1e-12, 1e-8) << "\n"; // compare "almost 0.0" to 0.0
+}
+
+bool _04_Old_01_Operators::ApproximatelyEqualAbsRel(double a, double b, double absEpsilon, double relEpsilon)
+{
+	// Check if the numbers are really close -- needed when comparing numbers near zero.
+	double diff = fabs(a - b);
+	if (diff <= absEpsilon)
+		return true;
+
+	// Otherwise fall back to Knuth's algorithm
+	return diff <= ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * relEpsilon);
+}
 
 int _04_Old_01_Operators::Add(int x, int y)
 {
 	return x + y;
+}
+
+
+void _04_Old_01_Operators::LogicalOperators()
+{
+	int x = 5;
+	int y = 7;
+
+	if (!x == y)
+		std::cout << "x does not equal y";
+	else
+		std::cout << "x equals y";
+
+	std::cout << '\n';
+
+	if (!(x == y))
+		std::cout << "x does not equal y";
+	else
+		std::cout << "x equals y";
+
+	std::cout << '\n';
+
+	std::cout << "Enter a number: ";
+	int value;
+	std::cin >> value;
+
+	if (value == 0 || value == 1)
+		std::cout << "You picked 0 or 1" << std::endl;
+	else
+		std::cout << "You did not pick 0 or 1" << std::endl;
+
+	std::cout << '\n';
+}
+
+void _04_Old_01_Operators::BitwiseOperators()
+{
+	//3 = 0011
+	//3 << 1 = 0110 = 6
+	//3 << 2 = 1100 = 12
+	//3 << 3 = 1000 = 8
+	//12 = 1100
+	//12 >> 1 = 0110 = 6
+	//12 >> 2 = 0011 = 3
+	//12 >> 3 = 0001 = 1
+	std::cout << sizeof(2048)<<' '<<sizeof(true)<<std::endl;
+
+	int x = 4;
+	std::cout << x<<std::endl;
+	x = x << 1;
+	std::cout << x<<std::endl;
 }
--- MyLearnCPP01/_04_Old_01_Operators.h
+++ MyLearnCPP01/_04_Old_01_Operators.h
@@ -6,5 +6,10 @@ public:
 	static void Test();
 	static void IncrementDecrement();
 	static void SizeofCommaConditional();
+	static void RelationalOperators();
+	static void EqualFloat();
+	static bool ApproximatelyEqualAbsRel(double, double, double = 1e-12, double = 1e-8);
 	static int Add(int x, int y);
+	static void LogicalOperators();
+	static void BitwiseOperators();
 };
