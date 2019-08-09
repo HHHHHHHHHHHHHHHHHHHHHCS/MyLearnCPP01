f2957e301405750abddf89d40cbe3c33e0e1e6b9
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-08-09 20:36:27 +0800

    temp

--- MyLearnCPP01/_06_07_FunctionPointers.cpp
+++ MyLearnCPP01/_06_07_FunctionPointers.cpp
@@ -3,7 +3,8 @@
 
 void _06_07_FunctionPointers::Test()
 {
-	Test01();
+	//Test01();
+	Test02();
 }
 
 void _06_07_FunctionPointers::Test01()
@@ -16,7 +17,7 @@ void _06_07_FunctionPointers::Test01()
 	std::cout << funcPtr() << '\n';
 
 	double (*douFunc)(double) = Test01_Func3;
-	std::cout << (*Test01_Func3)(8) << ' ' << Test01_Func3(4) << '\n';
+	std::cout << (*Test01_Func3)(8) << ' ' << Test01_Func3(4)<<' '<< douFunc(4) << '\n';
 }
 
 
@@ -33,4 +34,74 @@ int _06_07_FunctionPointers::Test01_Func2()
 double _06_07_FunctionPointers::Test01_Func3(double val)
 {
 	return val * val;
+}
+
+void _06_07_FunctionPointers::Test02()
+{
+	int array[] = { 3,7,9,5,6,1,8,2,4 };
+
+	Test02_SelectionSort(array, std::size(array));
+	Test02_PrintArray(array, std::size(array));
+
+	Test02_SelectionSort(array, std::size(array),Test02_Myscending);
+	Test02_PrintArray(array, std::size(array));
+}
+
+void _06_07_FunctionPointers::Test02_SelectionSort(int* array, size_t size, bool (*comparisonFunc)(int, int))
+{
+	for (int startIndex = 0; startIndex < size; startIndex++)
+	{
+		int smallestIndex = startIndex;
+
+		for (int currentIndex = startIndex + 1; currentIndex < size; currentIndex++)
+		{
+			if (comparisonFunc(array[smallestIndex], array[currentIndex]))
+			{
+				smallestIndex = currentIndex;
+			}
+		}
+		std::swap(array[startIndex], array[smallestIndex]); //C++11
+	}
+}
+
+bool _06_07_FunctionPointers::Test02_Ascending(int x, int y)
+{
+	return x > y;
+}
+
+bool _06_07_FunctionPointers::Test02_Descending(int x, int y)
+{
+	return x < y;
+}
+
+bool _06_07_FunctionPointers::Test02_Myscending(int x, int y)
+{
+	bool b1 = (x & 1) == 0;
+	bool b2 = (y & 1) == 0;
+
+	if (b1 && b2)
+	{
+		return x > y;
+	}
+	else if (!b1 && !b2)
+	{
+		return x < y;
+	}
+	else if (b1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void _06_07_FunctionPointers::Test02_PrintArray(int* array, size_t size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << array[i] << ' ';
+	}
+	std::cout << '\n';
 }
--- MyLearnCPP01/_06_07_FunctionPointers.h
+++ MyLearnCPP01/_06_07_FunctionPointers.h
@@ -8,4 +8,12 @@ public:
 	static int Test01_Func1();
 	static int Test01_Func2();
 	static double Test01_Func3(double val);
+
+
+	static void Test02();
+	static void Test02_SelectionSort(int *array, size_t size,bool(*comparisonFunc)(int,int) = Test02_Ascending);
+	static bool Test02_Ascending(int x, int y);
+	static bool Test02_Descending(int x, int y);
+	static bool Test02_Myscending(int x, int y);
+	static void Test02_PrintArray(int *array, size_t size);
 };
