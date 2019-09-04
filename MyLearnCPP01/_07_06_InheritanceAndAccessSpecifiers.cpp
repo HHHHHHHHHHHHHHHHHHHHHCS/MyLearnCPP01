e38d74b59310a91d202344d1842bbc8d25d0758c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-04 20:24:32 +0800

    add

---
+++ MyLearnCPP01/_07_06_InheritanceAndAccessSpecifiers.cpp
@@ -0,0 +1,47 @@
+﻿#include "_07_06_InheritanceAndAccessSpecifiers.h"
+
+void _07_06_InheritanceAndAccessSpecifiers::Test01()
+{
+	_07_06_InheritanceAndAccessSpecifiers_Cls::Base base;
+	base.m_public = 1;
+	//base.m_private = 2;
+	//base.m_protected = 3;
+
+	_07_06_InheritanceAndAccessSpecifiers_Cls::Derived derived;
+	derived.m_public = 1;
+	//derived.m_private = 2;
+	//derived.m_protected = 3;
+}
+
+void _07_06_InheritanceAndAccessSpecifiers::Test02()
+{
+	_07_06_InheritanceAndAccessSpecifiers_Cls::Pub pub;
+	pub.m_public = 1; // 
+	//pub.m_private = 2; // error
+	//pub.m_protected = 3; // error
+}
+
+void _07_06_InheritanceAndAccessSpecifiers::Test03()
+{
+	_07_06_InheritanceAndAccessSpecifiers_Cls::Pri pri;
+	//pri.m_public = 1; // 
+	//pri.m_private = 2; // error
+	//pri.m_protected = 3; // error
+}
+
+void _07_06_InheritanceAndAccessSpecifiers::Test04()
+{
+	_07_06_InheritanceAndAccessSpecifiers_Cls::Pro pro;
+	//pro.m_public = 1; // error
+	//pro.m_private = 2; // error
+	//pro.m_protected = 3; // error
+}
+
+void _07_06_InheritanceAndAccessSpecifiers::Test05()
+{
+	_07_06_InheritanceAndAccessSpecifiers_Cls::Def def;
+	//def.m_public = 1; 
+	//def.m_private = 2; 
+	//def.m_protected = 3; 
+}
+
---
+++ MyLearnCPP01/_07_06_InheritanceAndAccessSpecifiers.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+class _07_06_InheritanceAndAccessSpecifiers
+{
+public:
+	static void Test01();
+	static void Test02();
+	static void Test03();
+	static void Test04();
+	static void Test05();
+};
+
+class _07_06_InheritanceAndAccessSpecifiers_Cls
+{
+public:
+	class Base
+	{
+	public:
+		int m_public;
+	private:
+		int m_private;
+	protected:
+		int m_protected;
+	};
+
+	class Derived : public Base
+	{
+	public:
+		Derived()
+		{
+			m_public = 1;
+			//m_private = 2;//error
+			m_protected = 3;
+		}
+	};
+
+	class Pub : public Base
+	{
+	public:
+		Pub()
+		{
+			m_public = 1;
+			//m_private = 2;//error
+			m_protected = 3;
+		}
+	};
+
+	class Pri : private Base
+	{
+	public:
+		Pri()
+		{
+			m_public = 1;
+			//m_private = 2;//error
+			m_protected = 3;
+		}
+	};
+
+	class Pro : protected Base
+	{
+	public:
+		Pro()
+		{
+			m_public = 1;
+			//m_private = 2;//error
+			m_protected = 3;
+		}
+	};
+
+	class Def : Base //默认是private
+	{
+	public:
+		Def()
+		{
+			m_public = 1;
+			//m_private = 2;//error
+			m_protected = 3;
+		}
+	};
+};
