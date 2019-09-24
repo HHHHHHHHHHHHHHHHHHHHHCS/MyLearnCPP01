0a063923b0b5c0b30c414bd2e93c352a092604a1
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-24 20:14:15 +0800

    TEMP

---
+++ MyLearnCPP01/_08_01_ArithmeticOperators.cpp
@@ -0,0 +1,41 @@
+﻿#include "_08_01_ArithmeticOperators.h"
+#include <iostream>
+using Cents = _08_01_ArithmeticOperators::Cents;
+
+
+void _08_01_ArithmeticOperators::Test()
+{
+	Test01();
+}
+
+
+Cents operator+(const Cents& c1, const Cents& c2)
+{
+	return Cents(c1.m_cents + c2.m_cents);
+}
+
+Cents operator-(const Cents& c1, const Cents& c2)
+{
+	return Cents(c1.m_cents - c2.m_cents);
+}
+
+void _08_01_ArithmeticOperators::Test01()
+{
+	Cents cents1(6);
+	Cents cents2(8);
+
+	Cents centsSum = cents1 + cents2;
+	std::cout << centsSum.GetCents() << std::endl;
+
+	Cents centsSub = cents1 - cents2;
+	std::cout << centsSub.GetCents() << std::endl;
+
+	Cents centsMul = cents1 * cents2;
+	std::cout << centsMul.GetCents() << std::endl;
+
+	//TODO:Overloading operators for operands of different types
+}
+
+void _08_01_ArithmeticOperators::Test02()
+{
+}
---
+++ MyLearnCPP01/_08_01_ArithmeticOperators.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+class _08_01_ArithmeticOperators
+{
+public:
+	class Cents
+	{
+	private:
+		int m_cents;
+	public:
+		Cents(int cents) { m_cents = cents; }
+
+		friend Cents operator+(const Cents& c1, const Cents& c2);
+
+		friend Cents operator-(const Cents& c1, const Cents& c2);
+
+		friend Cents operator*(const Cents& c1, const Cents& c2)
+		{
+			return Cents(c1.GetCents() * c2.GetCents());
+		}
+
+
+		int GetCents() const { return m_cents; }
+	};
+
+
+	static void Test();
+	static void Test01();
+	static void Test02();
+};
