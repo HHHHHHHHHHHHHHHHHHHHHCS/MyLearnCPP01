3af9002b494714f22a42ac8b8b82dc4d890ff376
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-01-06 19:39:28 +0800

    add

---
+++ MyLearnCPP01/_10_04_InheritanceAccessSpecifiers.cpp
@@ -0,0 +1,24 @@
+﻿#include "_10_04_InheritanceAccessSpecifiers.h"
+#include <iostream>
+
+void _10_04_InheritanceAccessSpecifiers::Test()
+{
+	Base base{};
+	std::cout<<base.m_public;
+
+	Pub pub{};
+	std::cout << pub.m_public;
+	std::cout << pub.m_public1;
+
+	Pri pri{};
+	//std::cout << pri.m_public;//error
+	std::cout << pri.m_public1;
+
+	Pro pro;
+	//std::cout << pro.m_public;//error
+	std::cout << pro.m_public1;
+
+	Def def;
+	//std::cout << def.m_public;//error
+	std::cout << def.m_public1;
+}
---
+++ MyLearnCPP01/_10_04_InheritanceAccessSpecifiers.h
@@ -0,0 +1,91 @@
+﻿#pragma once
+
+class _10_04_InheritanceAccessSpecifiers
+{
+public:
+	class Base
+	{
+	public :
+		int m_public;
+	private:
+		int m_private;
+	protected:
+		int m_protected;
+	};
+
+	class Pub : public Base
+	{
+	public:
+		int m_public1;
+	private:
+		int m_private1;
+	protected:
+		int m_protected1;
+
+	public:
+		Pub()
+		{
+			m_public = 0;
+			//m_private = 1;//error
+			m_protected = 2;
+		}
+	};
+
+	class Pri : private Base
+	{
+	public:
+		int m_public1;
+	private:
+		int m_private1;
+	protected:
+		int m_protected1;
+
+	public:
+		Pri()
+		{
+			m_public = 0;
+			//m_private = 1;//error
+			m_protected = 2;
+		}
+	};
+
+	class Pro : protected Base
+	{
+	public:
+		int m_public1;
+	private:
+		int m_private1;
+	protected:
+		int m_protected1;
+
+	public:
+		Pro()
+		{
+			m_public = 0;
+			//m_private = 1;//error
+			m_protected = 2;
+		}
+	};
+
+
+	class Def : Base
+	{
+	public:
+		int m_public1;
+	private:
+		int m_private1;
+	protected:
+		int m_protected1;
+
+	public:
+		Def()
+		{
+			m_public = 0;
+			//m_private = 1;//error
+			m_protected = 2;
+		}
+	};
+
+
+	static void Test();
+};
