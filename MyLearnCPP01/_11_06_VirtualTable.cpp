9a74a2393b60e2a67c0800b6d8c5a44030061438
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-03-04 10:14:28 +0800

    add

---
+++ MyLearnCPP01/_11_06_VirtualTable.cpp
@@ -0,0 +1,18 @@
+﻿#include "_11_06_VirtualTable.h"
+void _11_06_VirtualTable::Test()
+{
+	Test01();
+}
+
+void _11_06_VirtualTable::Test01()
+{
+	//C++存在一个虚表 用于存放虚拟方法
+	//当D1时候  虚表 在编译阶段 被解析成D1
+	//所以 就算转换成Base 也不会调用基函数
+
+	//但是虚表存在虚拟指针  所以性能消耗会更大
+	D1 d1;
+	Base *dPtr = &d1;
+	dPtr->function1();
+}
+
---
+++ MyLearnCPP01/_11_06_VirtualTable.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+class _11_06_VirtualTable
+{
+public:
+	class Base
+	{
+	public:
+		//FunctionPointer *__vptr;
+		virtual void function1() {};
+		virtual void function2() {};
+	};
+
+	class D1 : public Base
+	{
+	public:
+		virtual void function1() {};
+	};
+
+	class D2 : public Base
+	{
+	public:
+		virtual void function2() {};
+	};
+	static void Test();
+	static void Test01();
+};
